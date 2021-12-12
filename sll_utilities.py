d8e1982ce1aab7fd878e5df6c2daf59db774d5c2
Ivana Vucenovic <ivucenovic@hotmail.com>
2021-12-12 07:38:37 -0500

    4

--- sll_utilities.py
+++ sll_utilities.py
@@ -16,19 +16,49 @@ class SLL:
         self.head = new_node
         return self
 
-    def printVals(self):
+    def contains(self, value):
+        runner = self.head
+        while runner != None:
+            if runner.value == value:
+                return True
+            runner = runner.next
+            return False
+        return self
+
+# contains(value) {
+#         var runner = this.head;
+#         while(runner) {
+#             if(runner.value === value) {
+#                 return true;
+#             }
+#             runner = runner.next;
+#         }
+#         return false;
+#     }
+    # Create a new SLL method length() that returns number of nodes in that list.
+    def length(self):
+        count = 0
+        runner = self.head
+        while runner != None:
+            count += 1
+            runner = runner.next
+        print(count)
+        return self
+        
+
+
+    # Create display() that returns a string containing all list values. Build what you wish console.log(myList) did!
+    def display(self):
         if self.head == None:
-            print("Linked list is empty")
-            return
-
-        llstr = ""
-
-        while self.head != None:
-            llstr += str(self.head.value)
-            self.head = self.head.next
-            return llstr
-        print(llstr)
-
+            print("No values")
+        else:
+            runner = self.head
+            myList = []
+            while runner != None:
+                print(runner.value)
+                # myList.append(runner.value)
+                runner = runner.next
+        return self
 
 sll = SLL()
 sll.addFront(4)
@@ -36,6 +66,8 @@ sll.addFront(8)
 sll.addFront(2)
 sll.addFront(6)
 
-sll.printVals()
-# sll.contains(4)
+# sll.printVals()
+sll.contains(4)
+sll.length()
+sll.display()
 
